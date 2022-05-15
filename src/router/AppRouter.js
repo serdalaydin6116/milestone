862cf8cbef984d572323cceef11ebe355c78dd89
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-16 00:47:43 +0300

    private router component was set

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,61 +1,40 @@
-import { useState } from "react";
-import {
-  BrowserRouter,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import Navbar from "../components/Navbar.js";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { Login } from "../pages/LoginRegister";
+import { Register } from "../pages/LoginRegister";
 import Dashboard from "../pages/Dashboard";
-import Profile from "../pages/Profile";
-import About from "../pages/About";
-import NewBlog from "../pages/NewBlog.js";
-import Details from "../pages/Details.js";
 import UpdateBlog from "../pages/UpdateBlog";
-import { useAuth } from "../contexts/AuthContext";
+import Detail from "../pages/Detail";
 
-function AppRouter() {
-  const [isAuth, setIsAuth] = useState(true);
-  const { currentUser } = useAuth();
+import NewBlog from "../pages/NewBlog";
+import Profile from "../pages/Profile";
+import PrivateRouter from "./PrivateRouter";
 
-  function PrivateRouter() {
-    let location = useLocation();
-
-    if (!currentUser) {
-      // Redirect them to the /login page, but save the current location they were
-      // trying to go to when they were redirected. This allows us to send them
-      // along to that page after they login, which is a nicer user experience
-      // than dropping them off on the home page.
-      return <Navigate to="/login" state={{ from: location }} replace />;
-    } else {
-      return <Outlet />;
-    }
-  }
-
+const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route
-          path="/login"
-          element={<Login setIsAuth={setIsAuth} isAuth={isAuth} />}
-        />
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-        <Route element={<PrivateRouter />}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="new-blog" element={<NewBlog />} />
-          <Route path="update-blog/:id" element={<UpdateBlog />} />
-          <Route path="detail/:id" element={<Details />} />
+
+        <Route path="/profile" element={<PrivateRouter />}>
+          <Route path="" element={<Profile />} />
+        </Route>
+        <Route path="/new-blog" element={<PrivateRouter />}>
+          <Route path="" element={<NewBlog />} />
+        </Route>
+        <Route path="/update-blog/:id" element={<PrivateRouter />}>
+          <Route path="" element={<UpdateBlog />} />
+        </Route>
+        <Route path="/detail/:id" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default AppRouter;
