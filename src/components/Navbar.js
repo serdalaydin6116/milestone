fa625348f3e87b50523415df19383cf3a5f9abfa
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-16 00:44:46 +0300

    navbar was synchronized

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,27 @@
 import React from "react";
-import { fade, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import { useAuth } from "../contexts/AuthContext";
+import cwLogo from "../assets/cw.jpeg";
 import { Link, useNavigate } from "react-router-dom";
-import cwJpeg from "../assets/cw.jpeg";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/thunks/authThunk";
+import { clearCurrentUserAction } from "../redux/actions/authActions";
 
 const useStyles = makeStyles((theme) => ({
-  grow: {
+  root: {
     flexGrow: 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
+    flexGrow: 1,
     display: "none",
     fontFamily: "Girassol",
     [theme.breakpoints.up("sm")]: {
@@ -29,193 +32,136 @@ const useStyles = makeStyles((theme) => ({
       color: "wheat",
     },
   },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
+  appBar: {
+    backgroundColor: "#046582",
   },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  logo: {
+    width: 40,
   },
-  inputRoot: {
-    color: "inherit",
+  linkStyle: {
+    textDecoration: "none",
+    color: "black",
   },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
-    },
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
-  loginRegDiv: {
-    display: "flex",
-    justifyContent: "space-between",
-    cursor: "pointer",
-  },
+
   login: {
     padding: 10,
     fontSize: 20,
     color: "white",
     textDecoration: "none",
   },
-  register: {
-    fontSize: 20,
-    padding: 10,
-    color: "white",
-    textDecoration: "none",
-  },
-  maxWidthLogReg: {
-    textDecoration: "none",
-    color: "black",
-  },
-  cwImg: {
-    width: 40,
-  },
-  appBar: {
-    backgroundColor: "#046582",
-  },
-  linkStyle: {
-    textDecoration: "none",
-    color: "black",
-  },
 }));
 
-export default function PrimarySearchAppBar() {
+export default function Navbar() {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const { currentUser, logout } = useAuth();
+  const open = Boolean(anchorEl);
+
+  const { currentUser } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
-  const isMenuOpen = Boolean(anchorEl);
-
-  const handleProfileMenuOpen = (event) => {
+  const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMenuClose = () => {
+  const handleClose = () => {
     setAnchorEl(null);
   };
 
   const handleLogout = () => {
-    handleMenuClose();
+    setAnchorEl(null);
     logout();
+    dispatch(clearCurrentUserAction());
   };
-
   const handleDashboard = () => {
+    setAnchorEl(null);
     navigate("/");
   };
 
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <>
-      {currentUser?.email ? (
-        <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          id={menuId}
-          keepMounted
-          transformOrigin={{ vertical: "top", horizontal: "right" }}
-          open={isMenuOpen}
-          onClose={handleMenuClose}
-          style={{ marginTop: "2rem", marginLeft: "1rem" }}
-        >
-          <Link to="/profile" className={classes.linkStyle}>
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-          </Link>
-          <Link to="/new-blog" className={classes.linkStyle}>
-            <MenuItem onClick={handleMenuClose}>New</MenuItem>
-          </Link>
-          <MenuItem onClick={handleLogout}>Logout</MenuItem>
-        </Menu>
-      ) : (
-        <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          id={menuId}
-          keepMounted
-          transformOrigin={{ vertical: "top", horizontal: "right" }}
-          open={isMenuOpen}
-          onClose={handleMenuClose}
-          style={{ marginTop: "2rem", marginLeft: "1.25rem" }}
-        >
-          <Link to="/login" className={classes.linkStyle}>
-            <MenuItem onClick={handleMenuClose}>Login</MenuItem>
-          </Link>
-          <Link to="/register" className={classes.linkStyle}>
-            <MenuItem onClick={handleMenuClose}>Register</MenuItem>
-          </Link>
-        </Menu>
-      )}
-    </>
-  );
-
   return (
-    <div className={classes.grow}>
-      <AppBar position="fixed" className={classes.appBar}>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
-            aria-label="open drawer"
+            aria-label="menu"
             onClick={handleDashboard}
           >
-            <img src={cwJpeg} alt="cw" className={classes.cwImg} />
+            <img src={cwLogo} alt="logo" className={classes.logo} />
           </IconButton>
-          <div className={classes.grow} />
-          <Link to="/" className={classes.login}>
-            <Typography className={classes.title} variant="h6" noWrap>
-              ──── <span>{"<ed8en/>"}</span> Blog ────
-            </Typography>
-          </Link>
-          <div className={classes.grow} />
+          <div className={classes.root}>
+            <Link to="/" className={classes.login}>
+              <Typography variant="h6" className={classes.title}>
+                ──── <span>{"<Clarusway IT />"}</span> BLOG ────
+              </Typography>
+            </Link>
+          </div>
+
           <div>
             <IconButton
-              edge="end"
               aria-label="account of current user"
-              aria-controls={menuId}
+              aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
+              onClick={handleMenu}
               color="inherit"
             >
-              <AccountCircle />
+              <AccountCircle style={{ fontSize: "40px" }} />
             </IconButton>
+            {currentUser?.email ? (
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <Link to="/profile" className={classes.linkStyle}>
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                </Link>
+                <Link to="/new-blog" className={classes.linkStyle}>
+                  <MenuItem onClick={handleClose}>New Blog</MenuItem>
+                </Link>
+                <Link to="/login" className={classes.linkStyle}>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Link>
+              </Menu>
+            ) : (
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <Link to="/login" className={classes.linkStyle}>
+                  <MenuItem onClick={handleClose}>Login</MenuItem>
+                </Link>
+                <Link to="/register" className={classes.linkStyle}>
+                  <MenuItem onClick={handleClose}>Register</MenuItem>
+                </Link>
+              </Menu>
+            )}
           </div>
         </Toolbar>
       </AppBar>
-
-      {renderMenu}
     </div>
   );
 }
