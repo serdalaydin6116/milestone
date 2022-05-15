142f49a9f8fa87d588cecb9cdef1fb8afde3630c
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-16 00:53:59 +0300

    app.js was synched

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import AppRouter from "./app-router/AppRouter";
-import { AuthProvider } from "./contexts/AuthContext";
-import { BlogProvider } from "./contexts/BlogContext";
+import "./App.css";
+import AppRouter from "./router/AppRouter";
 import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setCurrentUserAction } from "./redux/actions/authActions";
+import { auth } from "./utils/firebaseUtil";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      dispatch(setCurrentUserAction(user));
+    });
+    return unsubscribe; //! clean-up function
+  }, [dispatch]);
+
   return (
-    <AuthProvider>
-      <BlogProvider>
-        <AppRouter />
-        <ToastContainer />
-      </BlogProvider>
-    </AuthProvider>
+    <div className="App">
+      <AppRouter />
+      <ToastContainer />
+    </div>
   );
 }
 
 export default App;
+
 
