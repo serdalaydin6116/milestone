749c14e2360e0847720df4173da337ddc2d6e1e1
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-16 00:43:39 +0300

    blogform was reseted

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,34 +1,26 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import blogPng from "../assets/blok.png";
+import {
+  setNewBlogAction,
+  setUpdatedBlogAction,
+} from "../redux/actions/blogActions";
+
+import { useDispatch, useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
   avatar: {
     padding: theme.spacing(13),
     backgroundColor: "#046582",
   },
-  blogImg: {
-    width: 200,
-  },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%",
     marginTop: theme.spacing(3),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0),
     backgroundColor: "#046582",
     color: "white",
     fontWeight: "bold",
@@ -43,79 +35,99 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function BlogForm({ newBlog, setNewBlog, newBlogHandler }) {
+const BlogForm = ({ blog, handler }) => {
   const classes = useStyles();
+  const [newBlog, setNewBlog] = useState(blog);
+  const dispatch = useDispatch();
+  const { updatedBlog } = useSelector((state) => state.blog);
+
+  useEffect(() => {
+    setNewBlog(blog);
+  }, [blog]);
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <img src={blogPng} alt="blog" className={classes.blogImg} />
-        </Avatar>
-        <Typography component="h1" variant="h5" className={classes.title}>
-          ── New Blog ──
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="title"
-                label="Title"
-                name="title"
-                value={newBlog.title}
-                autoFocus
-                onChange={(e) =>
-                  setNewBlog({ ...newBlog, title: e.target.value })
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="image"
-                label="Image URL"
-                type="text"
-                id="image"
-                value={newBlog.image}
-                onChange={(e) =>
-                  setNewBlog({ ...newBlog, image: e.target.value })
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                id="outlined-multiline-static"
-                required
-                label="Content"
-                multiline
-                value={newBlog.content}
-                fullWidth
-                rows={15}
-                onChange={(e) =>
-                  setNewBlog({ ...newBlog, content: e.target.value })
-                }
-                // defaultValue="Default Value"
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
+    <form
+      className={classes.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+        handler(newBlog);
+      }}
+    >
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
             fullWidth
-            variant="contained"
-            className={classes.submit}
-            onClick={newBlogHandler}
-          >
-            Submit
-          </Button>
-        </form>
-      </div>
-    </Container>
+            required
+            id="title"
+            label="Title"
+            name="title"
+            value={newBlog?.title ?? ""}
+            autoFocus
+            onChange={(e) => {
+              //!local state updated
+              setNewBlog({ ...newBlog, title: e.target.value });
+              //!Global updatedBlog state updated
+              dispatch(
+                setUpdatedBlogAction({ ...updatedBlog, title: e.target.value })
+              );
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="image"
+            label="Image URL"
+            type="text"
+            id="image"
+            value={newBlog?.image ?? ""}
+            onChange={(e) => {
+              setNewBlog({ ...newBlog, image: e.target.value });
+              dispatch(
+                setUpdatedBlogAction({ ...updatedBlog, image: e.target.value })
+              );
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-multiline-static"
+            label="Content"
+            name="content"
+            multiline
+            required
+            value={newBlog?.content ?? ""}
+            onChange={(e) => {
+              setNewBlog({ ...newBlog, content: e.target.value });
+              dispatch(
+                setUpdatedBlogAction({
+                  ...updatedBlog,
+                  content: e.target.value,
+                })
+              );
+            }}
+            fullWidth
+            minRows={15}
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        className={classes.submit}
+      >
+        Submit
+      </Button>
+    </form>
   );
-}
+};
+
+export default BlogForm;
